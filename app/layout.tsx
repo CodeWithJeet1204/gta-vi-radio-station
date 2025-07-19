40e37585673de406b5769a9d372bb56c91a1503a
Jeet Jain <codewithjeet1204@gmail.com>
2025-07-19 13:41:40 +0530

    analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,13 @@
 import './globals.css'
 import { satoshi } from '@/app/fonts'
+import { Analytics } from "@vercel/analytics/next"
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={satoshi.variable}>
       <body className="bg-gta-dark text-white font-satoshi antialiased">
         {children}
+        <Analytics />
       </body>
     </html>
   );
